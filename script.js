ff14ace4e9175705186f5b623e728cfc0f07c030
Linda Lichtenstein <llichten@optonline.net>
2021-05-18 13:16:29 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+principalAmount = document.getElementById("principal").value;
+alert(principalAmount);
+if (principalAmount <= 0) 
+    alert("Please enter a positive number.");
+
+
+        function showValue(newValue)
+{
+    document.getElementById("InterestRate").innerHTML=newValue;
+}
+
+        
+
+
+
+function compute()
+{
+    p = document.getElementById("principal").value;
+
+}
